44aede9849250f0bd5ad684fd6f43a0a5460272e
Duyi-Wang <duyi.wang@intel.com>
2023-02-28 13:40:01 +0800

    Use tee to save log

--- benchmark_tf_fp32.sh
+++ benchmark_tf_fp32.sh
@@ -3,6 +3,6 @@
 cd "$( dirname "${BASH_SOURCE[0]}" )" || exit
 script_path="${PWD}"
 
-LD_PRELOAD="${script_path}"/libjemalloc.so.2.5.1 python deepfm.py --steps 3000 --no_eval --tf --parquet_dataset false --data_location /root/data/ > "${script_path}"/deepfm.log 2>&1
+LD_PRELOAD="${script_path}"/libjemalloc.so.2.5.1 python deepfm.py --steps 3000 --no_eval --tf --parquet_dataset false --data_location /root/data/ 2>&1 | tee "${script_path}"/deepfm.log 
 
 python log_process.py --log "${script_path}"/deepfm.log
